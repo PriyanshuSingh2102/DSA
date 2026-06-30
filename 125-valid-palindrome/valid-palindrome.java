3b825bbe6f865446c5ceb10a8a754f7f9a823e6c
PriyanshuSingh2102 <priyanshusingh2102@gmail.com>
2026-06-30 22:00:17 +0530

    Time: 244 ms (5.18%) | Memory: 48 MB (7.45%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,11 +1,13 @@
 class Solution {
     public boolean isPalindrome(String s) {
+
         int len = s.length();
 
         if (s.equals(" "))
             return true;
 
-        String str = "", rev = "";
+        String str = "";
+        String rev = "";
 
         for (int i = 0; i < len; i++) {
             if (Character.isLetterOrDigit(s.charAt(i))) {
