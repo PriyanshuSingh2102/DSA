10e46563992d506832a1b6d91b47108a16021d24
PriyanshuSingh2102 <priyanshusingh2102@gmail.com>
2026-07-04 19:43:22 +0530

    Time: 1 ms (100.00%) | Memory: 47.7 MB (14.11%) - LeetSync

---
+++ 978-valid-mountain-array/valid-mountain-array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+        int len = arr.length - 1;
+        int i = 0;
+
+        // Increasing
+        while (i < len && arr[i] < arr[i + 1]) {
+            i++;
+        }
+
+        if (i == 0 || i == len) {
+            return false;
+        }
+
+        while (i < len && arr[i] > arr[i + 1]) {
+            i++;
+        }
+
+        return i == len;
+    }
+}
