6955e1b268aa1f1dad070c00936a675c88155d1d
PriyanshuSingh2102 <priyanshusingh2102@gmail.com>
2026-07-05 20:01:57 +0530

    Time: 6 ms (87.53%) | Memory: 47.3 MB (55.13%) - LeetSync

---
+++ 1210-mean-of-array-after-removing-some-elements/mean-of-array-after-removing-some-elements.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+    public double trimMean(int[] arr) {
+        Arrays.sort(arr);
+        
+        int n = arr.length;
+        int k = n * 5 / 100;
+
+        double sum = 0;
+        for (int i = k; i < n - k; i++) {
+            sum += arr[i];
+        }
+
+        return sum / (n - 2 * k);
+    }
+}
