006b9264192cd297a6371813e748edaedb5bbdb5
PriyanshuSingh2102 <priyanshusingh2102@gmail.com>
2026-07-06 20:20:35 +0530

    Time: 6 ms (2.67%) | Memory: 43.4 MB (7.41%) - LeetSync

---
+++ 1848-sum-of-unique-elements/sum-of-unique-elements.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int sumOfUnique(int[] nums) {
+       int sum=0;
+       Arrays.sort(nums);
+       for(int i=0;i<nums.length;i++){
+        int c=0;
+        for(int j=0;j<nums.length;j++){
+             if(nums[i]==nums[j]) c++;
+        }
+        if(c==1)
+        sum+=nums[i];
+       } 
+       return sum;
+    }
+}
