51e58dc48bbb8fb5170b8c355ed6af1d406883c4
PriyanshuSingh2102 <priyanshusingh2102@gmail.com>
2026-07-07 19:33:56 +0530

    Time: 0 ms (100.00%) | Memory: 46.4 MB (62.06%) - LeetSync

---
+++ 2181-smallest-index-with-equal-value/smallest-index-with-equal-value.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int smallestEqual(int[] nums) {
+        int ind=-1;
+        for(int i=0;i<nums.length;i++){
+            if(i%10 == nums[i])
+             {
+                ind=i;
+                break;
+             }
+        }
+        return ind;
+    }
+}
