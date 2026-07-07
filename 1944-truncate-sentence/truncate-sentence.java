b89dbbf4e5c7fc933518823f7f8e9eedf34e0aa6
PriyanshuSingh2102 <priyanshusingh2102@gmail.com>
2026-07-07 13:45:36 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (58.60%) - LeetSync

--- 1944-truncate-sentence/truncate-sentence.java
+++ 1944-truncate-sentence/truncate-sentence.java
@@ -1,18 +1,15 @@
 class Solution {
     public String truncateSentence(String s, int k) {
-        int c=0;
-        s=s+" ";
-        String str="";
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)!=' ')
-             str+=s.charAt(i);
-             else{
-                c++;
-                str+=" " ;
-             }
-             if(c==k)
-                 break;
+        int spaces = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ')
+                spaces++;
+
+            if (spaces == k)
+                return s.substring(0, i);
         }
-        return str.trim();
+
+        return s;
     }
 }
